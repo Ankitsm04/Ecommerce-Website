5b9d413f451c1cbed66d61cac80486b0b93a2d06
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2024-08-11 20:57:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,57 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors')
+const cors = require('cors');
 const fileUpload = require('express-fileupload');
 require('dotenv').config();
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
 const app = express();
-app.use(cors(
-    {
-        origin: ["https://a1-market-backend.vercel.app"],
-        method: ["POST","GET","DELETE","PUT"],
-        credentials: true
-    }
-));
-// Middleware to parse JSON bodies
+
+// Middleware
+app.use(cors({
+    origin: ["https://a1-market-backend.vercel.app"],
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: '/tmp/'
 }));
-
-// Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000;
+// Routes
+app.use('/user', require('./routes/userRoutes'));
+app.use('/api', require('./routes/categoryRoutes'));
+app.use('/api', require('./routes/productRoutes'));
+app.use('/api', require('./routes/uploadRoutes'));
 
-app.get('/',(req,res)=>{
-    res.json({msg:"This is Example"})
-})
-
-app.listen(PORT,() =>{
-    console.log("Server Running")
-})
-
-
-
-//Routes
-app.use('/user',require('./routes/useRoutes'))
-app.use('/api',require('./routes/categoryRoutes'))
-app.use('/api',require('./routes/productRoutes'))
-app.use('/api',require('./routes/upload'))
+app.get('/', (req, res) => {
+    res.json({ msg: "This is Example" });
+});
 
 const URI = process.env.MONGODB_URI;
-
-mongoose.connect(URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    console.log("MongoDB Connected");
-    console.log("SERVER AT", PORT)
-}).catch(err=>{
-    console.log(err);
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-
+.then(() => {
+    console.log("MongoDB Connected");
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("Server Running");
+    });
+})
+.catch(err => {
+    console.log(err);
+});
