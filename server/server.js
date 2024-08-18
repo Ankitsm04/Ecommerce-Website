32fcf21c9df4c60f687e0b66d6148b5b17491901
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2024-08-18 17:18:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const cors = require('cors'); // Import cors package
 require('dotenv').config();
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+// CORS configuration to allow all origins
+app.use(cors({
+    origin: 'https://a1-market.vercel.app/', // Allow all origins
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true // Allow credentials if needed
+}));
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cookieParser());
@@ -19,36 +29,33 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
-    res.json({msg:"This is Example"})
-})
+app.get('/', (req, res) => {
+    res.json({ msg: "This is Example" });
+});
 
-app.listen(PORT,() =>{
-    console.log("Server Running")
-})
-
-
-
-//Routes
-app.use('/user',require('./routes/useRoutes'))
-app.use('/api',require('./routes/categoryRoutes'))
-app.use('/api',require('./routes/productRoutes'))
-app.use('/api',require('./routes/upload'))
+// Define routes
+app.use('/user', require('./routes/useRoutes'));
+app.use('/api', require('./routes/categoryRoutes'));
+app.use('/api', require('./routes/productRoutes'));
+app.use('/api', require('./routes/upload'));
 
 const URI = process.env.MONGODB_URI;
 
 const connectWithRetry = () => {
     mongoose.connect(URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      maxPoolSize: 10
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        maxPoolSize: 10
     }).then(() => {
-      console.log('MongoDB connected');
+        console.log('MongoDB connected');
     }).catch(err => {
-      console.error('MongoDB connection error', err);
-      setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
+        console.error('MongoDB connection error', err);
+        setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
     });
-  };
-  
-  connectWithRetry();
+};
 
+connectWithRetry();
+
+app.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`);
+});
