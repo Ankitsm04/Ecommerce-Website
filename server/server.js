d7990a5c2fcd8b1ff1a1e7e452ed8869d28865e9
Ankitsm38 <156781349+Ankitsm04@users.noreply.github.com>
2024-08-22 18:28:25 +0530

    new

--- server/server.js
+++ server/server.js
@@ -6,13 +6,15 @@ const fileUpload = require('express-fileupload')
 const cors = require('cors');
 
 
+
 const app = express();
 app.use(cors({
-    origin: ['https://a1-market.vercel.app', 'http://localhost:3000'],
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true
 }));
+
 app.use(express.json())             
 app.use(cookieParser())
 app.use(fileUpload({
